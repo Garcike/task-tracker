40445849c119003a284796d538523196557400e7
Miguel <miggarcia338@gmail.com>
2022-07-08 21:40:49 -0500

    adding initialize spring project

---
+++ backend/src/test/java/com/miguel/tracking/TaskTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
