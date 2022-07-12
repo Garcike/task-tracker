07f212503ab20f97908f50acb6a40369095df374
Miguel <miggarcia338@gmail.com>
2022-07-12 14:34:06 -0500

    adding methods to ProfileController

--- backend/src/main/java/com/miguel/tracking/controllers/ProfileController.java
+++ backend/src/main/java/com/miguel/tracking/controllers/ProfileController.java
@@ -1,9 +1,16 @@
 package com.miguel.tracking.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.miguel.tracking.models.Profile;
 import com.miguel.tracking.services.ProfileService;
 
 @RestController
@@ -17,6 +24,37 @@ public class ProfileController {
 		this.ps = ps;
 	}//end constructor
 	
+
+	@PostMapping("/login")
+	public ResponseEntity<Profile> login(String username, String password){
+		
+		Profile profile = ps.login(username, password);
+		
+		return new ResponseEntity<>(profile, HttpStatus.ACCEPTED);
+		
+		
+	}//end login
 	
+	@PostMapping("/register")
+	public ResponseEntity<Profile> createProfile(@RequestBody Profile profile){
+		
+		Profile newProfile = ps.createProfile(profile);
+		
+		return new ResponseEntity<>(newProfile, HttpStatus.CREATED);
+		
+	}//end createProfile
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Profile> getProfileByPid(@PathVariable("id") int id){
+		
+		Profile profile = ps.getProfileByPid(id);
+		
+		return new ResponseEntity<>(profile, HttpStatus.ACCEPTED);
+		
+	}//end getProfileByPid
+	
+	/*
+	 * Some methods to update and delete profile
+	 */
 	
 }//end profileController
