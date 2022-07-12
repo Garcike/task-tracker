67f0dfff1c7262c69a5fce78a6669bb5340c0266
Miguel <miggarcia338@gmail.com>
2022-07-12 14:20:54 -0500

    adding methods to ProfileController

--- backend/src/main/java/com/miguel/tracking/controllers/ProfileController.java
+++ backend/src/main/java/com/miguel/tracking/controllers/ProfileController.java
@@ -1,5 +1,22 @@
 package com.miguel.tracking.controllers;
 
-public class ProfileController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-}
+import com.miguel.tracking.services.ProfileService;
+
+@RestController
+@RequestMapping("/profile")
+public class ProfileController {
+	
+	private ProfileService ps;
+	
+	@Autowired
+	public ProfileController(ProfileService ps) {
+		this.ps = ps;
+	}//end constructor
+	
+	
+	
+}//end profileController
